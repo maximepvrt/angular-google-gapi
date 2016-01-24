3d12715c6b2a7e76a2ac27cb867eee4c04618a2e
Maxime Pauvert <maximepvrt@gmail.com>
2016-01-24 20:12:16 +0100

    fix #39

--- src/factories/GAuth.factory.js
+++ src/factories/GAuth.factory.js
@@ -41,7 +41,7 @@
             function offline() {
                 var deferred = $q.defer();
                 var origin = $location.protocol() + "//" + $location.hostname();
-                if($location.port() != "") {
+                if($location.port() != "" || ($location.port() != 443 && $location.protocol()== "https")) {
                     origin = origin + ':' + $location.port();
                 }
                 var win =  $window.open('https://accounts.google.com/o/oauth2/auth?scope='+encodeURI(SCOPE)+'&redirect_uri=postmessage&response_type=code&client_id='+CLIENT_ID+'&access_type=offline&approval_prompt=force&origin='+origin, null, 'width=800, height=600');
